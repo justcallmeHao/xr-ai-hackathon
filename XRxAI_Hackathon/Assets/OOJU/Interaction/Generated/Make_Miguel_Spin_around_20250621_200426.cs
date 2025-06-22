a93ff886f06ccc22d8018b374d6bd83036292afb
jade <jade.park@ooju.world>
2025-06-23 00:24:47 +0200

    feat: Package plugin files and resolve security issues
    Plugin files have been pre-packaged. This commit addresses security vulnerabilities while maintaining the previous functionality.

--- XRxAI_Hackathon/Assets/OOJU/Interaction/Generated/Make_Miguel_Spin_around_20250621_200426.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Make_Miguel_Spin_around_20250621_200426 : MonoBehaviour
-{
-    void Update()
-    {
-        transform.Rotate(Vector3.up, 100f * Time.deltaTime); // Spin Miguel around his y-axis
-    }
-}
